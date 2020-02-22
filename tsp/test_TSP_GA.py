198e29b4fc0680a18ba5945ac2f7394457b59df4
wenbin huang <ben@wenbins-MacBook-Pro.local>
2020-02-22 12:28:24 -0800

    debug the code, and add the test

--- tsp/test_TSP_GA.py
+++ tsp/test_TSP_GA.py
@@ -1,7 +1,9 @@
 import unittest
-
+import random
 from tsp_genetic import Genetic_TSP
 class MyTestCase(unittest.TestCase):
+
+
 
     def test_cross_over(self):
         parent1 = [3,4,8,2,7,1,6,5]
@@ -10,13 +12,75 @@ class MyTestCase(unittest.TestCase):
         correct_child2 = [4,6,5,2,7,1,3,8]
         start = 3
         end =  6
-        g = Genetic_TSP()
+        g = Genetic_TSP(9)
 
         child1 = g.gen_new_child(parent1, start, end, parent2[start:end])
         self.assertEqual(all([correct_child1[i] == child1[i] for i in range(len(correct_child1))]), True)
 
         child2 = g.gen_new_child(parent2, start, end, parent1[start:end])
         self.assertEqual(all([correct_child2[i] == child2[i] for i in range(len(correct_child2))]), True)
+
+
+    def cal_tsp_distance(self, gene, city_distance_data):
+        distance = 0
+        previous_city_no = 0
+        for cur_city_no in gene:
+            distance += city_distance_data[previous_city_no][cur_city_no]
+            previous_city_no = cur_city_no
+
+        distance += city_distance_data[previous_city_no][0]
+
+        return distance
+
+    def brute_force_tsp(self, city, data, permutation):
+        if len(city) == 0:
+            return self.cal_tsp_distance(permutation, data)
+        distance = 10**9
+        for gene in city:
+            city_copy = city[:]
+            permutation_copy = permutation[:]
+            permutation_copy.append(gene)
+            city_copy.remove(gene)
+            res = self.brute_force_tsp(city_copy, data, permutation_copy)
+            distance = min(res, distance)
+
+        return distance
+
+    def test_tsp_ga_with_small_data(self):
+        city_encoding = {0:"Bakersfield", 1:"Barstow", 2:"Carlsbad", 3:"Eureka", 4:"Fresno", 5:"Lake Tahoe, So."}
+        data = [[  0,129,206,569,107,360],
+                [129, 0,153,696,236,395],
+                [206,153, 0,777,315,780],
+                [569,696,777, 0,462,398],
+                [107,236,315, 462,0,388],
+                [360,395,780,398,388, 0]]
+
+        g = Genetic_TSP(data, city_number = 6)
+
+        best = g.run(500)
+        print(best.fitness)
+        print(best.gene)
+        min_distance = self.brute_force_tsp([1,2,3,4,5], data, [])
+        print("min_distance= " + str(min_distance))
+
+
+    ## todo continues to fill the data
+    def test_tsp_ga_with_big_data(self):
+        city_encoding = {0:"Bakersfield", 1:"Barstow", 2:"Carlsbad", 3:"Eureka", 4:"Fresno", 5:"Lake Tahoe, So."}
+        data = [[0, 129,206,569,107,360,284,144,115,162,200,231,288,226,436,272,174,231,297,252,118,146,258,347,121,227,200],
+                [0, 0,  153,696,236,395,155,139,130,291,329,360,417,123,565,401, 71,176,426,381,247,225,387,476,250,356,329],
+                [0, 0,    0,777,315,780,312, 82, 93,370,406,428,496,116,644,480,827, 23,505,460,293,188,466,565,329,435,408],
+                [0, 0,    0,  0,462,398,797,713,694,407,369,388,291,795,150,314, 43,800,272,317,504,609,349,222,544,356,488],
+                [107,236,315,462,0,388],
+                [360,395,780,398,388, 0]]
+
+        g = Genetic_TSP(data, city_number = 6)
+
+        best = g.run(500)
+        print(best.fitness)
+        print(best.gene)
+        min_distance = self.brute_force_tsp([1,2,3,4,5], data, [])
+        print("min_distance= " + str(min_distance))
 
 if __name__ == '__main__':
     unittest.main()
--- tsp/tsp_genetic.py
+++ tsp/tsp_genetic.py
@@ -4,8 +4,8 @@ class Indivisual():
     def __init__(self, gene):
         self.gene = gene
         self.fitness = None
-
-## todo do the final test 
+## todo do the final test
+## todo add selection rate?
 class Genetic_TSP():
     ## the curcuit always begins from the 0 city
     def __init__(self, city_distance_data = None,
@@ -15,16 +15,17 @@ class Genetic_TSP():
         self.city_number = city_number
         self.cross_rate = cross_rate
         self.mutation_rate = mutation_rate
-        self.population = []
         self.population_count = population_count
         self.init_population_randomly(city_number, population_count)
         self.k = 7
+        self.population = []
 
     def init_population_randomly(self, city_num, population_count):
         for i in range(population_count):
             gene = range(1, city_num)
-            random_gene = random.shuffle(gene)
-            self.population.append(random_gene)
+            random.shuffle(gene)
+            ind = self.gen_indivisual(gene)
+            self.population.append(ind)
 
 
     def cal_tsp_distance(self, gene):
@@ -36,13 +37,14 @@ class Genetic_TSP():
 
         distance += self.city_distance_data[previous_city_no][0]
 
+        return distance
+
     def fitness(self, indivisual):
         indivisual.fitness = self.cal_tsp_distance(indivisual.gene)
 
     def cal_k_lowest_indivisual_chosen(self):
-
         self.population.sort(key=lambda x: x.fitness)
-        return self.population[:self.k]
+        return self.population[:self.k + 1]
 
 
     def select(self):
@@ -55,9 +57,9 @@ class Genetic_TSP():
 
         return new_population
 
-    def gen_crossover_position(self):
-        start = random.randrange(0, self.city_number)
-        end = random.randrange(start + 1, self.city_number)
+    def gen_crossover_position(self, gen_len):
+        start = random.randrange(0, gen_len)
+        end = random.randrange(start, gen_len)
         return (start, end)
 
     def get_parent_random(self):
@@ -68,31 +70,44 @@ class Genetic_TSP():
     def gen_new_child(self, parent, start, end, replace_gene):
         replace_gene_set = set(replace_gene)
         origin_gene = parent[start: end]
+        remove_val = []
         for city_no in origin_gene:
             if city_no in replace_gene_set:
-                origin_gene.remove(city_no)
+                remove_val.append(city_no)
+        for val in remove_val:
+            origin_gene.remove(val)
 
         child = parent[:start]
         child.extend(replace_gene)
         child.extend(parent[end:])
         origin_gene_idx = 0
+        ## todo bug here
         for i in range(0, start):
             if child[i] in replace_gene_set:
+                if origin_gene_idx >= len(origin_gene):
+                    print("here")
                 child[i] = origin_gene[origin_gene_idx]
                 origin_gene_idx += 1
 
         for i in range(end, len(child)):
             if child[i] in replace_gene_set:
+                if origin_gene_idx >= len(origin_gene):
+                    print("here")
                 child[i] = origin_gene[origin_gene_idx]
                 origin_gene_idx += 1
 
+        if self.is_valid_child(child) == False:
+            print("here")
         return child
 
+    def is_valid_child(self, gene):
+        s = set(gene)
+        return len(gene) == len(s)
     def next_generation(self):
         self.population = self.select()
 
         new_generation = []
-        for i in range(self.city_number/2):
+        for i in range(self.population_count/2):
             child1, chil2 = self.crossover()
             new_generation.extend([child1, chil2])
 
@@ -101,24 +116,32 @@ class Genetic_TSP():
 
         self.population = new_generation
 
+    def gen_indivisual(self, child_gene):
+        i = Indivisual(child_gene)
+        i.fitness = self.cal_tsp_distance(child_gene)
+
+        return i
     def crossover(self):
         r = random.uniform(0, 1)
 
-        start, end = self.gen_crossover_position()
         parent1 = self.get_parent_random()
         parent2 = self.get_parent_random()
         if r > self.cross_rate:
             return (parent1, parent2)
 
-        child1 = self.gen_new_child(parent1, start, end, parent2[start, end])
-        child2 = self.gen_new_child(parent2, start, end, parent1[start, end])
+        parent1_gene = parent1.gene
+        parent2_gene = parent2.gene
 
-        return (child1, child2)
+        start, end = self.gen_crossover_position(len(parent1_gene))
+        child1 = self.gen_new_child(parent1_gene, start, end, parent2_gene[start:end])
+        child2 = self.gen_new_child(parent2_gene, start, end, parent1_gene[start:end])
+
+        return (self.gen_indivisual(child1), self.gen_indivisual(child2))
 
     def mutation(self, indivisual):
         r = random.uniform(0, 1)
         if r < self.mutation_rate:
-            start, end = self.gen_crossover_position()
+            start, end = self.gen_crossover_position(len(indivisual.gene))
 
             indivisual.gene[start], indivisual.gene[end] = indivisual.gene[end], indivisual.gene[start]
 
@@ -126,11 +149,9 @@ class Genetic_TSP():
         return self.cal_k_lowest_indivisual_chosen()[0]
 
     def run(self, iteration_times):
-        ga = Genetic_TSP()
-
         i = 0
         while i < iteration_times:
-            ga.next_generation()
+            self.next_generation()
             i += 1
 
-        return ga.best()
+        return self.best()
